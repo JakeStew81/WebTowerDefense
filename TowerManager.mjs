3be1e59892bab0439d94bae51267645e7c41dcbb
JakeStew81 <jakestewart711@icloud.com>
2024-06-03 22:02:42 -0400

    Further balance changes

--- TowerManager.mjs
+++ TowerManager.mjs
@@ -41,7 +41,7 @@ export class TowerManager {
         this.ghostFunctions.set("longRangeTower", (event) => this.displayTowerGhost(this.longRangeTowerGhost, event));
 
         this.towerStats = new Map();
-        this.towerStats.set("normalTower", [5, 65, 4.5, 100]);
+        this.towerStats.set("normalTower", [5, 70, 4.5, 100]);
         this.towerStats.set("fastTower", [3, 18, 4, 125]);
         this.towerStats.set("boxingTower", [15, 75, 1.5, 150]);
         this.towerStats.set("longRangeTower", [5, 80, 999, 150]);
